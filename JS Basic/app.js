c8304eb7cbfcc2c9ad7cb751c1d2f98215f6c6b2
er-mmm10 <ermm10051995@gmail.com>
2022-06-03 11:02:02 +0530

    functions

--- JS Basic/app.js
+++ JS Basic/app.js
@@ -50,3 +50,53 @@
 // console.log(typeof value2);
 // console.log(typeof result);
 // console.log(typeof res);
+
+// value vs reference
+// const num = 2;
+// let num2 = num;
+// console.log(num, num2);
+// num2 = 7;
+// console.log(num, num2);
+
+// //reference
+// const arr = [1, 2, 3];
+// let arr2 = arr;
+// console.log(arr, arr2);
+// arr2[2] = 4;
+// console.log(arr, arr2);
+// _____________________________________________________________________________
+
+// 1. CallBack Functions. Higher Order Functions, Functions are first class objects/ Citizens
+// 2. Functions are first class objects/ Citizens - stored in a variable (expression), passed as an argument to another function, return from the function (closure).
+// 3. HOF: accepts another function as an argument or returns another function as a result
+// 4. Callback function - pased to another function as an argument and executed inside that function
+
+// function greetMorning(name) {
+//   const myName = "MM";
+//   console.log(`Good Morning ${name}, my name is ${myName}`);
+// }
+
+// function greetAfternoon(name) {
+//   const myName = "MM";
+//   console.log(`Good Afternoon ${name}, my name is ${myName}`);
+// }
+
+// greetMorning("Dikshit");
+// greetAfternoon("Raman");
+
+// better appraoch
+function morning(name) {
+  return `Good Morning ${name.toUpperCase()}`;
+}
+
+function afternoon(name) {
+  return `Good afternoon ${name.toUpperCase()}`;
+}
+
+function greet(name, cb) {
+  const myName = "MM";
+  console.log(` ${cb(name)}, my name is ${myName}`);
+}
+
+greet("Raman", morning);
+greet("Dikshit", afternoon);
